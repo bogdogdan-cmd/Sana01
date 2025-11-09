f832c58e782a8abdb2108aac1196c69e9667420d
bogdogdan-cmd <bogdogdan@gmail.com>
2025-11-09 22:07:54 +0200

    Sana01

---
+++ LinearExpressions1/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.Write("Введіть a: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.Write("Введіть b: ");
+double b = double.Parse(Console.ReadLine());
+
+Console.Write("Введіть c: ");
+double c = double.Parse(Console.ReadLine());
+
+Console.Write("Введіть d: ");
+double d = double.Parse(Console.ReadLine());
+
+double x = (a + 2 * b - c + d) / (c * d)+ (a + b) / (Math.Pow(c, 2))- Math.Pow(b, 2) / (2 * a);
+
+double y = (5 * (a + b) * (c - d)) / (Math.Pow(c, 2) - 2 * c * d + Math.Pow(d, 2))- (Math.Pow(b, 2) - 2 * Math.Pow(b, 2)) / (b - a);
+
+double z = (Math.Pow(Math.Pow(a, 2) - 2 * a, 3) - 4 * (Math.Pow(a, 4) + 1)) * (1 - b)/ 5 * a + 3 * b;
+
+double r = (Math.Pow(a, 2) + Math.Pow(b, 2)) / (3 * c + 1) + (1 / (a - c));
+Console.WriteLine("Значення x = {0}, y = {1}, z = {2}, r = {3}", x, y, z, r);
+
+
+
---
+++ LinearExpressions2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.Write("Введіть m: ");
+double m = double.Parse(Console.ReadLine());
+
+Console.Write("Введіть n: ");
+double n = double.Parse(Console.ReadLine());
+
+Console.Write("Введіть a: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.Write("Введіть b: ");
+double b = double.Parse(Console.ReadLine());
+
+Console.Write("Введіть x: ");
+double x = double.Parse(Console.ReadLine());
+
+double z1 = ((m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n))/ (Math.Sqrt(Math.Pow(m, 3)) + n * m + Math.Pow(m, 2) - m);
+double z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+double y = 2.4 * (Math.Abs((Math.Pow(x, 2) + b) / a)) + (a - b) * Math.Pow(Math.Sin(a - b), 2) + Math.Pow(10, -2) * (x - b);
+
+Console.WriteLine("Значення z1 = {0}, z2 = {1}, y = {2}", z1, z2, y);
